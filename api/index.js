ffc7afb38c5c6b33262ca59e1c958648ffdb6e5d
TULIMAT Anas OBS/GDO <TZKM2460@orange.com>
2023-08-12 12:07:49 +0200

    feat(back!): add regestration functionnality

--- api/index.js
+++ api/index.js
@@ -1,16 +1,40 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
+require('dotenv').config();
+
+const User = require('./models/User');
 
 const app = express();
 
+const bcryptSalt = bcrypt.genSaltSync(12);
+
+app.use(express.json());
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173',
 }));
 
+mongoose.connect(process.env.MONGO_URL);
+
 
 app.get('/test', (req, res) => {
     res.json('test ok');
+});
+
+app.post('/register', async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+        const user = await User.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcryptSalt)
+        })
+        res.json(user);
+    } catch (e) {
+        res.status(422).json(e);
+    }
 });
 
 app.listen(4000);
