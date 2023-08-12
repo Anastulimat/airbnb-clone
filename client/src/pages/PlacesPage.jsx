1871b7ba930e11b117f8b5c50bd32393052c4190
TULIMAT Anas OBS/GDO <TZKM2460@orange.com>
2023-08-12 22:24:34 +0200

    feat(front): [WIP] complete adding fields to new place form

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,7 +1,41 @@
 import {Link, useParams} from "react-router-dom";
+import {useState} from "react";
+import Perks from "../components/Perks.jsx";
 
 export default function PlacesPage() {
     const {action} = useParams();
+
+    const [title, setTitle] = useState('');
+    const [address, setAddress] = useState('');
+    const [addedPhotos, setAddedPhotos] = useState([]);
+    const [photoLink, setPhotoLink] = useState('');
+    const [description, setDescription] = useState('');
+    const [perks, setPerks] = useState([]);
+    const [extraInfo, setExtraInfo] = useState('');
+    const [checkIn, setCheckIn] = useState('');
+    const [checkOut, setCheckOut] = useState('');
+    const [maxGuests, setMaxGuests] = useState(1);
+
+    function inputHeader(header = '') {
+        return (
+            <h2 className="text-2xl mt-4">{header}</h2>
+        );
+    }
+
+    function inputDescription(description = '') {
+        return (
+            <p className="text-gray-500 text-sm">{description}</p>
+        );
+    }
+
+    function preInput(header = '', description = '') {
+        return (
+            <>
+                {inputHeader(header)}
+                {inputDescription(description)}
+            </>
+        )
+    }
 
 
     return (
@@ -23,23 +57,95 @@ export default function PlacesPage() {
             {action === 'new' && (
                 <div>
                     <form>
-                        <h2 className="text-2xl mt-4">Title</h2>
-                        <p className="text-gray-500 text-sm">Title for your place. should be short and catchy</p>
-                        <input type="text" name="title" id="title"
+                        {preInput('Title', 'Title for your place. should be short and catchy')}
+                        <input type="text"
+                               name="title"
+                               id="title"
+                               value={title}
+                               onChange={event => setTitle(event.target.value)}
                                placeholder="Title, for example: My lovely appartement"/>
 
-                        <h2 className="text-2xl mt-4">Address</h2>
-                        <p className="text-gray-500 text-sm">Address to this place</p>
-                        <input type="text" name="address" id="address" placeholder="Address"/>
+                        {preInput('Address', 'Address to this place')}
+                        <input type="text"
+                               name="address"
+                               id="address"
+                               value={address}
+                               onChange={event => setAddress(event.target.value)}
+                               placeholder="Address"/>
 
-                        <h2 className="text-2xl mt-4">Photos</h2>
-                        <p className="text-gray-500 text-sm">Show us how amazing your place is !</p>
+                        {preInput('Photos', 'Show us how amazing your place is !')}
+                        <div className="flex gap-2">
+                            <input type="text"
+                                   value={photoLink}
+                                   onChange={event => setPhotoLink(event.target.value)}
+                                   placeholder={'Add using a link ... jpg'}/>
+                            <button className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
+                        </div>
+
+                        {/* Upload photos from device part */}
                         <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-                            <button className="border bg-transparent rounded-2xl p-8 text-2xl text-gray-500">
-                                +
+                            <button
+                                className="flex justify-center gap-1 border bg-transparent rounded-2xl p-8 text-2xl text-gray-500">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                                    <path strokeLinecap="round" strokeLinejoin="round"
+                                          d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"/>
+                                </svg>
+
+                                Upload
                             </button>
                         </div>
 
+                        {preInput('Description', 'A good description of the place')}
+                        <textarea name="description"
+                                  id="description"
+                                  value={description}
+                                  onChange={event => setDescription(event.target.value)}
+                                  cols="10"
+                                  rows="5">
+                        </textarea>
+
+                        {preInput('Perks', 'Select all the perks of your place')}
+                        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-2 mt-2">
+                            <Perks selected={perks} onChange={setPerks}/>
+                        </div>
+
+                        {preInput('Extra info', 'House, rules, etc...')}
+                        <textarea name="extra-infp"
+                                  id="extra-info"
+                                  value={extraInfo}
+                                  onChange={event => setExtraInfo(event.target.value)}
+                                  cols="extra-info"
+                                  rows="5">
+                        </textarea>
+
+                        {preInput('Check in & out times, max guests', 'Add check in and out times, remember to have some time for cleaning the room')}
+                        <div className="grid gap-2 sm:grid-cols-3">
+                            <div>
+                                <h3 className="mt-2 -mb-1">Check in time</h3>
+                                <input type="text"
+                                       placeholder={"14"}
+                                       value={checkIn}
+                                       onChange={event => setCheckIn(event.target.value)}/>
+                            </div>
+                            <div>
+                                <h3 className="mt-2 -mb-1">Check out time</h3>
+                                <input type="text"
+                                       placeholder={"11"}
+                                       value={checkOut}
+                                       onChange={event => setCheckOut(event.target.value)}/>
+                            </div>
+                            <div>
+                                <h3 className="mt-2 -mb-1">Max number of guests</h3>
+                                <input type="number"
+                                       placeholder={"1"}
+                                       min="1"
+                                       value={maxGuests}
+                                       onChange={event => setMaxGuests(parseInt(event.target.value))}/>
+                            </div>
+                        </div>
+
+                        <button className="primary my-4">Save</button>
                     </form>
                 </div>
             )}
