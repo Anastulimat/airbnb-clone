9b552083db26108888bd71ecc5eb725de13b961a
TULIMAT Anas OBS/GDO <TZKM2460@orange.com>
2023-08-12 10:11:05 +0200

    feat(front): add login and register skeleton pages

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,17 +1,18 @@
 import {Link} from "react-router-dom";
 
-export default function LoginPage() {
+export default function RegisterPage() {
     return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64">
-                <h1 className="text-4xl text-center mb-4">Login</h1>
+                <h1 className="text-4xl text-center mb-4">Register</h1>
                 <form className="max-w-md mx-auto">
+                    <input type="text" name="name" id="name" placeholder={"John Doe"}/>
                     <input type="email" name="email" id="email" placeholder={"your@email.com"}/>
                     <input type="password" name="password" id="password" placeholder={"your password"}/>
                     <button className="primary">Login</button>
 
                     <div className="text-center py-2 text-gray-500">
-                        Don't have an account yet ? <Link to={'/register'} className="underline text-black">Register now</Link>
+                        Already a member? <Link to={'/login'} className="underline text-black">Login</Link>
                     </div>
                 </form>
             </div>
