fe46fe4b6af0ed7239bb2b68ca3372919aa3dc97
TULIMAT Anas OBS/GDO <TZKM2460@orange.com>
2023-08-12 09:31:37 +0200

    fix: adjust padding on layout

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <>
             {/* Logo part */}
-            <header className="p-4 flex justify-between">
+            <header className="flex justify-between">
                 <a href="/" className="flex items-center gap-1">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
                          stroke="currentColor" className="w-8 h-8 -rotate-90">
