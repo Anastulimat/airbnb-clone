cf3752c4c4f94333a3e5dc9a9591a70764c20079
TULIMAT Anas OBS/GDO <TZKM2460@orange.com>
2023-08-14 00:26:21 +0200

    feat(front+back): display places on index page

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,10 +1,33 @@
 import {Link} from "react-router-dom";
 import Header from "../components/Header.jsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 export default function IndexPage() {
+
+    const [places, setPlaces] = useState([]);
+
+    useEffect(() => {
+        axios.get('/places').then(response => {
+            setPlaces([...response.data, ...response.data, ...response.data, ...response.data, ...response.data, ...response.data, ...response.data, ...response.data, ...response.data, ...response.data]);
+        });
+    }, []);
+
     return (
-        <div>
-            Index page here
+        <div className="mt-16 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
+            {places.length > 0 && places.map(place => (
+                <div>
+                    <div className="bg-gray-500 mb-2 rounded-2xl flex">
+                        {place.photos?.[0] && (
+                            <img className="rounded-2xl object-cover aspect-square"
+                                 src={'http://localhost:4000/uploads/' + place.photos?.[0]}
+                                 alt=""/>
+                        )}
+                    </div>
+                    <h2 className="text-sm truncate leading-4">{place.title}</h2>
+                    <h3 className="font-bold">{place.address}</h3>
+                </div>
+            ))}
         </div>
     )
 }
