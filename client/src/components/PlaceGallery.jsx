b8e2bcb52f4c485d3e83787777e76abc589053e6
TULIMAT Anas OBS/GDO <TZKM2460@orange.com>
2023-08-15 14:48:43 +0200

    feat(front): add single booking page and do some refactoring

---
+++ client/src/components/PlaceGallery.jsx
@@ -0,0 +1,71 @@
+import {useState} from "react";
+
+export default function PlaceGallery({place}) {
+
+    const [showAllPhotos, setShowAllPhotos] = useState(false);
+
+    if (showAllPhotos) {
+        return (
+            <div className="absolute inset-0 bg-black text-white min-h-screen">
+                <div className="bg-black p-8 grid gap-4">
+                    <div>
+                        <h2 className="text-3xl">Photos of {place.title}</h2>
+                        <button onClick={() => setShowAllPhotos(false)}
+                                className="fixed right-12 top-8 flex gap-2 py-2 px-4 rounded-2xl bg-white text-black">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                                 stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"/>
+                            </svg>
+                            Close photos
+                        </button>
+                    </div>
+                    {place.photos?.length > 0 && place.photos.map(photo => (
+                        <div>
+                            <img src={'http://localhost:4000/uploads/' + photo} alt=""/>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div className="relative">
+            <div className="grid grid-cols-[2fr_1fr] gap-2 rounded-2xl overflow-hidden">
+                <div>
+                    {place.photos?.[0] && (<div>
+                        <img onClick={() => setShowAllPhotos(true)}
+                             className="aspect-square object-cover cursor-pointer"
+                             src={'http://localhost:4000/uploads/' + place.photos[0]}
+                             alt=""/>
+                    </div>)}
+                </div>
+                <div className="grid">
+                    {place.photos?.[1] && (
+                        <img onClick={() => setShowAllPhotos(true)}
+                             className="aspect-square object-cover cursor-pointer"
+                             src={'http://localhost:4000/uploads/' + place.photos[1]} alt=""/>)}
+
+                    <div className="overflow-hidden">
+                        {place.photos?.[2] && (<img onClick={() => setShowAllPhotos(true)}
+                                                    className="aspect-square object-cover cursor-pointer relative top-2"
+                                                    src={'http://localhost:4000/uploads/' + place.photos[2]}
+                                                    alt=""/>)}
+                    </div>
+                </div>
+
+            </div>
+            <button onClick={() => setShowAllPhotos(true)}
+                    className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-black">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                     stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round"
+                          d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"/>
+                </svg>
+
+                Show more photos
+            </button>
+        </div>
+    );
+
+}
--- client/src/pages/BookingPage.jsx
+++ client/src/pages/BookingPage.jsx
@@ -1,9 +1,47 @@
 import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import PlaceAddress from "../components/PlaceAddress.jsx";
+import PlaceGallery from "../components/PlaceGallery.jsx";
+import BookingDates from "../components/BookingDates.jsx";
 
 export default function BookingPage() {
     const {id} = useParams();
+    const [booking, setBooking] = useState(null);
+
+    useEffect(() => {
+        if (id) {
+            axios.get('/bookings').then(response => {
+                const foundBooking = response.data.find(({_id}) => _id === id);
+                if (foundBooking) {
+                    setBooking(foundBooking);
+                }
+            })
+        }
+    }, [id]);
+
+    if (!booking) {
+        return '';
+    }
 
     return (
-        <div>Single booking</div>
+        <div className="my-8">
+            <h1 className="text-2xl mr-48">{booking.place.title}</h1>
+            <PlaceAddress className="my-2 block">{booking.place.address}</PlaceAddress>
+            <div className="bg-gray-200 p-4 mb-4 rounded-2xl flex justify-between items-center">
+                <div>
+                    <h2 className="text-2xl mb-4">Your booking information:</h2>
+                    <BookingDates booking={booking}/>
+                </div>
+
+                <div className="bg-primary p-6 text-white rounded-2xl">
+                    <div>Total price</div>
+                    <div className="text-3xl">${booking.price}</div>
+                </div>
+            </div>
+
+
+            <PlaceGallery place={booking.place}></PlaceGallery>
+        </div>
     );
 }
